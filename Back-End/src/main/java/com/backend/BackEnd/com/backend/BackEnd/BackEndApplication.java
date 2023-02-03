89d5bf2b1a2d36fbcf925230dcf0141d8b315c53
Waleed Alhindi <wl392785@dal.ca>
2023-02-03 17:26:09 -0400

    Set up back-end spring boot repo

---
+++ Back-End/src/main/java/com/backend/BackEnd/com/backend/BackEnd/BackEndApplication.java
@@ -0,0 +1,14 @@
+package com.backend.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
