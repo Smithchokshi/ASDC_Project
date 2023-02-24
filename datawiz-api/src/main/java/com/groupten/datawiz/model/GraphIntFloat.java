fd6e4c52fe421542b493d09a74e14ed07db14a98
Siddhesh Nandkumar Salve <SD264348@dal.ca>
2023-02-24 10:05:39 -0400

    create model class to get User data for graph

---
+++ datawiz-api/src/main/java/com/groupten/datawiz/model/GraphIntFloat.java
@@ -0,0 +1,16 @@
+package com.groupten.datawiz.model;
+
+public class GraphIntFloat {
+
+    private int x;
+
+    private float y;
+
+    public int getX() { return x; }
+
+    public void setX(int x) { this.x = x; }
+
+    public float getY() { return y; }
+
+    public void setY(float y) { this.y = y; }
+}
---
+++ datawiz-api/src/main/java/com/groupten/datawiz/model/GraphStringFloat.java
@@ -0,0 +1,17 @@
+package com.groupten.datawiz.model;
+
+public class GraphStringFloat {
+
+
+    private String x;
+
+    private float y;
+
+    public String getX() { return x; }
+
+    public void setX(String x) { this.x = x; }
+
+    public float getY() { return y; }
+
+    public void setY(float y) { this.y = y; }
+}
